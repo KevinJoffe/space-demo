229c60b63dd99745cdbb1ea2e558d85946f0adc1
KevinJoffe <kevin@kreait.com>
2017-10-06 12:46:53 +0200

    feat: Update sun prefab, add new skybox

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-    
+public class GameManager : MonoBehaviour
+{
     public GameObject sun;
 
     public GameObject[] planets;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start()
+    {
         Instantiate(sun, sun.transform.position, Quaternion.identity);
 
-        for(int i = 0; i < planets.Length; i++)
+        for (int i = 0; i < planets.Length; i++)
         {
             GameObject planet = planets[i];
 
@@ -20,19 +19,18 @@ public class GameManager : MonoBehaviour {
 
             Instantiate(planet, getPlanetPosition(), Quaternion.identity);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
     private Vector3 getPlanetPosition()
     {
-
-        int x = Random.Range(-150, 150);
-        int y = Random.Range(-150, 150);
-        int z = Random.Range(-200, 50);
+        float x = Random.Range(-150, 150);
+        float y = Random.Range(-150, 150);
+        float z = Random.Range(-200, 50);
 
         return new Vector3(sun.transform.position.x + x, sun.transform.position.y + y, sun.transform.position.z + z);
     }
