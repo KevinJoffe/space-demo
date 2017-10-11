2bf0d1e585f72f0c8c0dcaec634d6dfe7fea86f5
KevinJoffe <kevin@kreait.com>
2017-10-11 16:51:23 +0200

    feat: limit planets spawn, fix sun spawning multiple times, clean game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,4 @@
 
 public class GameManager : MonoBehaviour
 {
-    private PlanetSpawner _planetSpawner;
-
-    private void Start()
-    {
-        _planetSpawner = new PlanetSpawner(new Planet.Factory(), new Sun.Factory());
-    }
 }
--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -1,22 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Zenject;
 
 public class PlanetSpawner : ITickable
 {
     readonly Planet.Factory _planetFactory;
     readonly Sun.Factory _sunFactory;
+    private List<Planet> planets;
+    private Sun sun;
 
     public PlanetSpawner(Planet.Factory planetFactory, Sun.Factory sunFactory)
     {
         Debug.Log("PlanetSpawnerConstructor");
         _planetFactory = planetFactory;
         _sunFactory = sunFactory;
+        planets = new List<Planet>();
     }
 
     public void Tick()
     {
         Debug.Log("Tick");
-        var sun = _sunFactory.Create();
-        _planetFactory.Create(sun.transform.position);
+
+        if (planets.Count == 0)
+        {
+            sun = _sunFactory.Create();
+            planets.Add(_planetFactory.Create(sun.transform.position));
+        }
+        else if (planets.Count < 100)
+        {
+            planets.Add(_planetFactory.Create(sun.transform.position));
+        }
     }
 }
